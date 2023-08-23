78e8c436100c0ba92e454bf5008dd683224eb8ac
yodit93 <yoditabebe9391@gmail.com>
2023-08-23 11:53:17 +0300

    Create sidebar form fields

---
+++ src/components/FormFields.jsx
@@ -0,0 +1,13 @@
+import { Space } from 'antd';
+const FormFields = () => {
+    return ( 
+        <Space direction="vertical">
+            <a href="#">Add Input Field</a>
+            <a href="#">Add Text Field</a>
+            <a href="#">Add Checkbox</a>
+            <a href="#">Add Selection</a>
+        </Space>
+     );
+}
+ 
+export default FormFields;
